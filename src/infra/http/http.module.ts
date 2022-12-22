7889ea10b19e39cec241efdbb13cf95954edaafb
Issac Cabral <issaccabrall@gmail.com>
2022-12-21 23:04:59 -0300

    feat: add http module

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { SendNotification } from "src/app/usecases/send-notification";
+import { DatabaseModule } from "../database/database.module";
+import { NotificationsController } from "./controllers/notifications.controller";
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [
+    SendNotification
+  ]
+})
+export class HttpModule {}
